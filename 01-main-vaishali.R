ceb9c7e8ee9d4f0f9651fbb84d09d7687022af9a
unknown <vaishalisvp@gmail.com>
2018-03-24 18:26:58 +0100

    TIME SERIES PLOT
    time series plot

--- 01-main-vaishali.R
+++ 01-main-vaishali.R
@@ -61,8 +61,13 @@ chisq.out.test(conngeo$duration)
 chisq.out.test(conngeo$orig_ip_bytes)
 chisq.out.test(conngeo$resp_ip_bytes)
 
+outliers <- conngeo %>%  filter(duration==2157.128511 | orig_ip_bytes==330177 |resp_ip_bytes==4433136) %>% select(id.orig_h)
+
+outliers
 
 ################################################################################
+
+weird <- data.table::fread("C:/Users/WINDOWS-PC/Dropbox/Datafest 2018/datafest2018_data_and_documentation/data/weird.csv")
 
 
 
@@ -83,7 +88,7 @@ b <- get_map(bbox)
 
 ggmap(b) + geom_point(data=korea, aes(lon, lat), size=2, alpha=0.7)
 
-#Calendar heat map
+#####################TIME SERIES PLOT#######################
 library(plyr)
 library(scales)
 library(zoo)
@@ -109,11 +114,16 @@ conngeo <- ddply(conngeo,.(yearmonthf), transform, monthweek=1+week-min(week))
 
 
 
-conn1 <- conngeo[, c("year", "yearmonthf", "monthf", "week", "monthweek", "day", "orig_ip_bytes")]
+conn1 <- conngeo[, c("week", "duration", "location", "yearmonthf")]
 
-head(conn1)
 
-plot1 <- ggplot2::ggplot(conn1, aes(monthweek, day, fill = orig_ip_bytes)) + 
+conn2 <- filter(conn1, location=="United States"|location=="Germany")
+ggplot(conn2, aes(week, duration, group=location, color=location)) + geom_line() +
+   xlab("") + ylab("Duration")
+
+#######################################################################################
+
+#plot1 <- ggplot2::ggplot(conn1, aes(monthweek, day, fill = orig_ip_bytes)) + 
   geom_tile(colour = "white") + 
   facet_grid(year~monthf) + 
   scale_fill_gradient(low="red", high="green") +
